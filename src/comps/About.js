e53ebbfd1af9bff3dfe967323332c98e45862b73
Mike Eling <mike.eling97@gmail.com>
2019-10-19 18:02:43 +0200

    :white_check_mark: Fix CI warnings

--- src/comps/About.js
+++ src/comps/About.js
@@ -1,25 +1,10 @@
 import React from "react";
-import { Container } from ".";
 
 const About = () => {
-    const [opacity, setOpacity] = React.useState(1.0);
     const contentRef = React.useRef(null);
 
-    React.useEffect(() => {
-        window.addEventListener("scroll", () => {
-            var st = window.scrollY - 100;
-            const limit = window.innerHeight + (contentRef.current ? contentRef.current.clientHeight - 150 : 200);
 
-            /* avoid unnecessary call to jQuery function */
-            if(st > limit) setOpacity(0);
-            if (st <= limit) {
-               setOpacity((1 - st/limit));
-            }
-        });
-    }, []);
-
-
-    return <div className="About" style={{opacity, height: contentRef.current ? contentRef.current.clientHeight - 150 : 200}}>
+    return <div className="About" style={{height: contentRef.current ? contentRef.current.clientHeight - 150 : 200}}>
         <div className="About__content" ref={contentRef}>
         <h1>About me</h1>
         <p>Nisi fugiat velit dolor incididunt amet quis laboris. Anim incididunt cillum occaecat laborum fugiat minim ea pariatur est ad ipsum nulla non. In sunt laboris voluptate enim aliquip ullamco esse fugiat proident. Officia esse cupidatat consectetur cupidatat nostrud aliquip. Enim irure commodo ea proident esse cillum anim minim ea. Do fugiat nisi aliquip laboris commodo amet tempor nostrud velit consectetur sit ut. Qui minim laborum adipisicing et voluptate incididunt adipisicing sit sunt consectetur officia in.</p>
