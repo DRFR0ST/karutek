600a666fc2e4f55667acda202b1f1e898aafa10c
Mike Eling <mike.eling97@gmail.com>
2019-10-20 15:35:49 +0200

    :card_file_box: Fill database

--- src/db.js
+++ src/db.js
@@ -2,13 +2,13 @@ export default {
   "posts": [
     {
       "id": 1,
-      "title": "Lorem ipsum",
+      "title": "Goodbye Summer",
       "description": "This work is inspired by the end of summer. A leaf on the surface of the pool water symbolizes the beginning of autumn. The project was made in Photoshop, using basic tools and digital drawing using a graphic tablet.",
       "category": "digital",
       "image": "https://i.imgur.com/URh64vJ.png",
       "date": "17.11.1995",
       "horizontal": false,
-      "color": "#99CC66",
+      "color": "#ffc9e7",
       "textColor": "#fff",
       "made_with": [
         "adobe_photoshop",
@@ -16,7 +16,7 @@ export default {
     },
     {
       "id": 2,
-      "title": "Lorem ipsum",
+      "title": "Sunset on desert",
       "description": "The work is inspired by the existing flagship illustration of Illustrator. I really liked the colors, gradients and atmosphere of this illustration, which is why I decided to recreate it, in order to improve my ability to work in illustrator.",
       "category": "digital",
       "image": "https://i.imgur.com/oY07uUF.png",
@@ -30,43 +30,41 @@ export default {
     },
     {
       "id": 3,
-      "title": "Lorem ipsum",
+      "title": "American breakfast",
       "description": "This work presents a simple illustration of breakfast done in the style of flat design. Made with simple Photoshop tools and digital drawing on a graphic tablet.",
       "category": "digital",
       "image": "https://i.imgur.com/O2LQbhQ.png",
       "date": "17.11.1995",
       "horizontal": false,
-      "color": "#fff",
+      "color": "#fffcbf",
       "textColor": "#000",
       "made_with": [
-        "adobe_photoshop",
-        "adobe_illustrator"
+        "adobe_photoshop"
       ]
     },
     {
       "id": 4,
-      "title": "Lorem ipsum",
+      "title": "Physiomedica LOGO",
       "description": "Logo designed for a physiotherapy clinic, of which I created the entire graphic design, from the logo, through the design of business cards, to the design of the external appearance of the clinic. Logo Created by hand using a graphic tablet. The name of the clinic was also proposed by me and I adapted the appropriate font and colors.",
       "category": "digital",
       "image": "https://i.imgur.com/XtNqCjT.png",
       "date": "17.11.1995",
       "horizontal": true,
-      "color": "#eee",
+      "color": "#bfedff",
       "textColor": "#000",
       "made_with": [
-        "adobe_photoshop",
-        "adobe_illustrator"
+        "adobe_photoshop"
       ]
     },
     {
       "id": 5,
-      "title": "Lorem ipsum",
+      "title": "Physiomedica business card",
       "description": "Physiotherapy clinic business card design. Simple, clean, minimalistic. With the preservation of colors, a consistent logo and basic information to preserve the medical character.",
       "category": "digital",
       "image": "https://i.imgur.com/Ptz0F16.png",
       "date": "17.11.1995",
       "horizontal": false,
-      "color": "#121212",
+      "color": "#ffc9e7",
       "textColor": "#fff",
       "made_with": [
         "adobe_photoshop"
@@ -74,13 +72,13 @@ export default {
     },
     {
       "id": 6,
-      "title": "Lorem ipsum",
+      "title": "3D Bedroom",
       "description": "3D bedroom design created to explore the cinema 4D program. A simple, uncomplicated design based on solids. working on this project was an interesting experience for me and great fun.",
       "category": "digital",
       "image": "https://i.imgur.com/BcOJDqL.png",
       "date": "17.11.1995",
       "horizontal": false,
-      "color": "#000",
+      "color": "#ffe2bf",
       "textColor": "#fff",
       "made_with": [
         "cinema_4d"
@@ -88,14 +86,17 @@ export default {
     },
     {
       "id": 7,
-      "title": "Lorem ipsum",
+      "title": "VOGUE drawing",
       "description": "Digital drawing made in photoshop using a graphic tablet. Made on the occasion of the birthday of a colleague who was the inspiration to create this drawing. it is not perfectly.",
       "category": "digital",
       "image": "https://i.imgur.com/j63AFHk.png",
       "date": "17.11.1995",
-      "color": "#212121",
+      "color": "#e6cfff",
       "textColor": "#fff",
-      "horizontal": false
+      "horizontal": false,
+      "made_with" : [
+        "adobe_photoshop"
+      ]
     }
   ]
 }
