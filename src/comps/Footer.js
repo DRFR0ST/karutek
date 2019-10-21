7561f0aff8e369aecf61497ce0764eb3a811aa50
Mike Eling <mike.eling97@gmail.com>
2019-10-21 13:10:10 +0200

    :bento: Remove help link

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -6,9 +6,6 @@ const Footer = () => {
     return <div className="Footer">
         <div className="Footer__content">
             <p className="Footer__copyright">Copyright © 2019 Karolina Falkowska</p>
-            <div className="Footer__right">
-                <a href="https://google.com/" alt="help">Pomoc</a>
-            </div>
         </div>
     </div>
 }
